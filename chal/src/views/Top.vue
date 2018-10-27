be94fd5dd7643124ef9d39a4859d8e99d7fc312b
sakasa <well.reverse@gmail.com>
2018-10-27 22:56:46 +0900

    vue-cliでtopを作り直し

---
+++ chal/src/views/Top.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="top">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-cell">
+      <header class="mdl-layout__header"></header>
+      <main class="mdl-layout__content mdl-grid">
+        <div class="page-content">
+          <h1>Chal</h1>
+          <div>
+            <button id="use_button" 
+              class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
+              @click="click()">
+              つかう
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from '../../src/router';
+export default {
+  name: 'Top',
+  methods: {
+    click() {
+      router.push('login')
+    }
+  }
+}
+</script>
